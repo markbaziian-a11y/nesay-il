849147cc05a150ff3a2d2b518e36637a7b3b64c1
Марк Базиян <markbazian@MacBook-Air-Mark.local>
2026-07-02 17:33:10 +0300

    fix referrals db path

--- src/routes/referrals.js
+++ src/routes/referrals.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
-const auth = require('../middleware/auth');
+const db = require('../../config/db');
+const auth = require('../../middleware/auth');
 
 router.get('/my', auth, async (req, res) => {
   try {
